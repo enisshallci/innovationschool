f749fc02bd7884bbeea677a314875dcbde1dd12f
Enis Shallci <enisshallci@gmail.com>
2024-11-19 13:16:30 +0100

    get students by id method added

--- src/main/java/org/javaboy/innovationschool/InnovationschoolApplication.java
+++ src/main/java/org/javaboy/innovationschool/InnovationschoolApplication.java
@@ -2,7 +2,9 @@ package org.javaboy.innovationschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+	Annotation i tregon Spring boot aplikacionit cfare beans me aktivizu, cfare komponenta me aktivizu.
+ */
 @SpringBootApplication		//gradle e din qe eshte spring boot application me kete annotation
 public class InnovationschoolApplication {
 
--- src/main/java/org/javaboy/innovationschool/students/StudentsController.java
+++ src/main/java/org/javaboy/innovationschool/students/StudentsController.java
@@ -1,13 +1,17 @@
 package org.javaboy.innovationschool.students;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController     //Per me tregu qe eshte RestController, spring boot e interpreton si restController.
 public class StudentsController {
 
-    //Forma e komunikimit te HTTP permes, ne path /students ne serverin tone do te thirret metoda getStudent()
-    @GetMapping(path = "/students")
+    /*
+        Forma e komunikimit te HTTP, ne path /students ne serverin tone do te thirret metoda getStudent()
+        Pra, Spring boot i mappon GET requests ne URL e specifikuar me "/students" me këtë metodë.
+     */
+    @GetMapping(path = "/students/{id}")
     public Student getStudent() {
 
         Student studentModel = new Student();
@@ -19,10 +23,10 @@ public class StudentsController {
         return studentModel;
     }
 
-    @GetMapping(path = "/trainings")
+    @PostMapping(path = "/trainings")
     public String getTraining() {
 
-        return "Spring Boot and Microservices";
+        return "Spring Boot and Microservices 2024";
     }
 
 }
