def2e9f80316222096a6fdee5642e98cda39c40c
Enis Shallci <enisshallci@gmail.com>
2024-12-01 11:48:12 +0100

    find students by name implemented

--- src/main/java/org/javaboy/innovationschool/students/StudentsRepository.java
+++ src/main/java/org/javaboy/innovationschool/students/StudentsRepository.java
@@ -3,9 +3,11 @@ package org.javaboy.innovationschool.students;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface StudentsRepository extends JpaRepository<StudentEntity, Long> {
 
-
+    Set<StudentEntity> findByFirstNameStartingWith(String name);    //metode e derivuar.
 
 }
