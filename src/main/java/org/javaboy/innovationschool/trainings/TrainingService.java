ab1251826ff3e2a884ac3ff32071bc2a04e4b01f
Enis Shallci <75337247+enisshallci@users.noreply.github.com>
2024-12-01 14:17:24 +0100

    Update TrainingService.java

--- src/main/java/org/javaboy/innovationschool/trainings/TrainingService.java
+++ src/main/java/org/javaboy/innovationschool/trainings/TrainingService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-public interface TrainingService {
+public interface TrainingsService {
 
     List<TrainingEntity>  findAll();
     Optional<TrainingEntity> findById(Long id);
